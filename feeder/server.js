de6e0a4da74aee2e9a0e75a69150a00529e2b170
Antonio <sago.ant@gmail.com>
2018-02-23 13:31:45 +0100

    Saved list novels in external json object

--- feeder/server.js
+++ feeder/server.js
@@ -2,18 +2,19 @@ const express = require('express')
 const axios = require('axios');
 const app = express()
 const port = process.env.PORT || 3000
+const novelList = require('./novelList.json')
 
-const randidlyFeedURL = "http://royalroadl.com/fiction/syndication/11209"
-const dragonFeedURL = "http://royalroadl.com/fiction/syndication/9904"
+
 const UpdateChaptersURL = "https://us-central1-westernnovelupdates.cloudfunctions.net/updateChapters"
 
 app.get('*', async (req, res) =>
 {
     try
     {
-        let feed = await axios.get(dragonFeedURL)
+        let novel = novelList[0]
+        let feed = await axios.get(novel["FeedUrl"])
         let updatedChaptersResponse = await axios.post(UpdateChaptersURL, feed.data, 
-            { headers: { "Content-Type": "text/plain", "Novel-ID": "i2yoWM7Nkbek0UqPgEMq" }})
+            { headers: { "Content-Type": "text/plain", "Novel-ID": novel["ID"] }})
 
         res.status(updatedChaptersResponse.status).send(updatedChaptersResponse.data)
     }
