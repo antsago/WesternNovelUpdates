08878943dddc7e02503f79fbab10149e12481ae4
Antonio <sago.ant@gmail.com>
2018-02-28 18:23:09 +0100

    Removed unused require

--- feeder/server.js
+++ feeder/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const axios = require('axios');
 const app = express()
 const port = process.env.PORT || 3000
-const novelList = require('./novelList.json')
 const admin = require('firebase-admin')
 
 const UpdateChaptersURL = "https://us-central1-westernnovelupdates.cloudfunctions.net/updateChapters"
