c1bb4a5baba5cff13c5c53294b3b7d9b3717cb01
Antonio <sago.ant@gmail.com>
2018-03-12 15:56:40 +0100

    Removed extra semicolons

--- webapp/src/app/core/database.service.ts
+++ webapp/src/app/core/database.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 import { AngularFirestore } from 'angularfire2/firestore'
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable'
 
 const CHAPTERS = 'chapters'
 const NOVELS = 'novels'
