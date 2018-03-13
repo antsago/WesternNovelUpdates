d7550a44f5b78cfa2ea947a0502f3506ec534d64
Antonio <sago.ant@gmail.com>
2018-03-13 18:46:56 +0100

    Fixed display error when waiting to retrieve novel detail

--- webapp/src/app/core/novelDetail.component.ts
+++ webapp/src/app/core/novelDetail.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute, Params } from '@angular/router'
 })
 export class NovelDetailComponent implements OnInit
 {
-    novel: {}
+    novel = {}
 
     constructor(private db: DatabaseService, private activatedRoute: ActivatedRoute) {}
 
