263ad6941e6556b5d5a1138b540e9f5c6fe258de
Antonio <sago.ant@gmail.com>
2018-04-06 12:35:09 +0200

    refactor: added barrel to shared module

---
+++ webapp/src/app/shared/index.ts
@@ -0,0 +1,7 @@
+export * from './user/authentication.service'
+export * from './user/login.service'
+export * from './alert.service'
+export * from './database.service'
+export * from './Interfaces'
+export * from './readingList.service'
+export * from './shared.module'
