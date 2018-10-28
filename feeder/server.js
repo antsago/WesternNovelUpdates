b5cdf1a7ada7c2c42b680b0739f9000647ec5503
Antonio <sago.ant@gmail.com>
2018-10-28 20:36:33 +0100

    fix(feeder): using a variable before declaring it

--- feeder/server.js
+++ feeder/server.js
@@ -2,9 +2,8 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 const admin = require('firebase-admin')
-const serviceAccount = InHeroku ? undefined : require('./serviceAccount.json')
-
 const InHeroku = process.env.InHeroku
+const serviceAccount = InHeroku ? undefined : require('./serviceAccount.json')
 
 const token = InHeroku ? process.env.token : serviceAccount.security_token
 const databaseURL = InHeroku ? process.env.FirebaseDatabaseURL : serviceAccount.database_url
